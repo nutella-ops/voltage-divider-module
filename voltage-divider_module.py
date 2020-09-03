c688e2792c7e056b1c855cbdd51570eb96e33468
nutella-ops <52546474+nutella-ops@users.noreply.github.com>
2020-09-03 12:31:27 -0700

    Add files via upload
    makin' computer do divider math 4 me yeet

---
+++ voltage-divider_module.py
@@ -0,0 +1,15 @@
+#!/Users/macbook/miniconda3/bin/python3
+k = 10^3
+M = 10^6
+
+
+vcc = input("Vcc = ")
+print(vcc) #test
+r1 = input("R1 = ")
+r2 = input("R2 = ")
+print(r2) #test
+tol = input("R tolerance = ")
+
+per = int(tol)/100
+print(per) #test
+
